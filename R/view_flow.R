482890820ff703f9548914e1f9a52e45c2af689d
Antoine Fabri <antoine.fabri@gmail.com>
2020-08-04 04:20:25 +0200

    comment out old view_flow

--- R/view_flow.R
+++ R/view_flow.R
@@ -1,46 +1,46 @@
-#' View function as flow chart
+#' #' View function as flow chart
+#' #'
+#' #' @param f
+#' #'
+#' #' @param prefix prefix to use for special comments, must start with `"#"`
+#' #' @param ... additional parameters passed to `build_nomnoml_code()`
+#' #'
+#' #' @export
+#' view_flow <- function(f, prefix = NULL, code = TRUE, png = NULL, width = NULL,
+#'                       height = NULL, output = c("view", "nomnoml", "data"),..., svg = FALSE){
+#'   ## nocov start
+#'   output <- match.arg(output)
+#'   f_sym <- substitute(f)
 #'
-#' @param f
+#'   # put comments in `#`() calls
+#'   f <- add_comment_calls(f, prefix)
+#'   # build data from the function body
+#'   data <- new_data()
+#'   data <- add_node(
+#'     data,
+#'     id = 0L,
+#'     "header",
+#'     code = f_sym,
+#'     code_str = deparse(f_sym))
+#'   data <- add_edge(data, from=0L, to = 1L)
+#'   data <- add_data_from_expr(data, body(f))
 #'
-#' @param prefix prefix to use for special comments, must start with `"#"`
-#' @param ... additional parameters passed to `build_nomnoml_code()`
+#'   # add the final node
+#'   id = get_last_id(data) + 1
+#'   data <- add_node(data, id, "return")
+#'   if(!is.null(prefix)){
+#'     prefix <- paste0("^\\s*", prefix,"\\s*")
+#'     data$nodes$label <- sub(prefix, "", data$nodes$label)
+#'   }
+#'   class(data) <- c("funflow", class(data))
+#'   if (output == "data") return(data)
+#'   code <- build_nomnoml_code(data, code = code, ...)
+#'   if (output == "nomnoml") return(code)
+#'   nomnoml::nomnoml(code, png = png, width = width, height = height, svg = svg)
+#'   ## nocov end
+#' }
+#' # cat(paste(strsplit(code,"\n")[[1]][1:400],collapse = "\n"))
 #'
-#' @export
-view_flow <- function(f, prefix = NULL, code = TRUE, png = NULL, width = NULL,
-                      height = NULL, output = c("view", "nomnoml", "data"),..., svg = FALSE){
-  ## nocov start
-  output <- match.arg(output)
-  f_sym <- substitute(f)
-
-  # put comments in `#`() calls
-  f <- add_comment_calls(f, prefix)
-  # build data from the function body
-  data <- new_data()
-  data <- add_node(
-    data,
-    id = 0L,
-    "header",
-    code = f_sym,
-    code_str = deparse(f_sym))
-  data <- add_edge(data, from=0L, to = 1L)
-  data <- add_data_from_expr(data, body(f))
-
-  # add the final node
-  id = get_last_id(data) + 1
-  data <- add_node(data, id, "return")
-  if(!is.null(prefix)){
-    prefix <- paste0("^\\s*", prefix,"\\s*")
-    data$nodes$label <- sub(prefix, "", data$nodes$label)
-  }
-  class(data) <- c("funflow", class(data))
-  if (output == "data") return(data)
-  code <- build_nomnoml_code(data, code = code, ...)
-  if (output == "nomnoml") return(code)
-  nomnoml::nomnoml(code, png = png, width = width, height = height, svg = svg)
-  ## nocov end
-}
-# cat(paste(strsplit(code,"\n")[[1]][1:400],collapse = "\n"))
-
-# tmp <- tempfile(fileext = ".pdf")
-# view_flow(data.table::fread, png = tmp)
-# br
+#' # tmp <- tempfile(fileext = ".pdf")
+#' # view_flow(data.table::fread, png = tmp)
+#' # br
