a65ecaef1dacd381d03fc0a2c859ee2c1736e210
Antoine Fabri <antoine.fabri@gmail.com>
2020-08-06 18:58:29 +0200

    refactor flow_run

--- R/06_flow_run.R
+++ R/06_flow_run.R
@@ -44,29 +44,25 @@ flow_run <-
     finished <- FALSE
     id <- 1
 
+    update_diagram <- function() {
+      # display updated diagram
+      nomnoml_code  <- build_nomnoml_code(data, code = code, ...)
+      widget_params <- list(code = nomnoml_code, svg = FALSE)
+      widg <- htmlwidgets::createWidget(
+        name = "nomnoml", widget_params, package = "nomnoml")
+      print(widg)
+    }
+
     # we display the diagram on exit, this way it will be displayed even if the
     # code fails
     on.exit({
-      code <- build_nomnoml_code(data, code = code, ...)
-      x <- list(code = code, svg = FALSE)
-      widg <- htmlwidgets::createWidget(
-        name = "nomnoml", x,
-        package = "nomnoml")
-      print(widg)
+      update_diagram()
     })
 
     if (browse) {
 
-      update_diagram <- function() {
-        # display updated diagram
-        nomnoml_code  <- build_nomnoml_code(data, code = code, ...)
-        widget_params <- list(code = nomnoml_code, svg = FALSE)
-        widg <- htmlwidgets::createWidget(
-            name = "nomnoml", widget_params, package = "nomnoml")
-        print(widg)
-      }
-
-      flow_browser <- function(id,e) {
+      flow_browser <- function(id,e, browse) {
+        if (isTRUE(browse) || id %in% browse)
         repeat {
           line <- readline(paste0("flow_browser[", id,"]> "))
           if (line == "") break
@@ -77,7 +73,7 @@ flow_run <-
       }
     }
 
-    while (!finished) {
+    repeat {
       # if we fall on an "end" node (they have negative indices), go to the next one
       if (id < 0) {
         ind <- with(data$edges, from == id & edge_label == "")
@@ -88,11 +84,14 @@ flow_run <-
       # flag node and extract row of node data from data$nodes into `node`
       data$nodes$entered[data$nodes$id == id] <- TRUE
       node <- as.list(data$nodes[data$nodes$id == id,])
+      # since we evaluate the code outside of the function, we should remove the
+      # return calls, so return(expr) becomes expr, the block_type info will
+      # take it from there
 
       if (browse) {
         update_diagram()
         cat("Next block: ",node$block_type, "\n", node$code_str, "\n")
-        flow_browser(id, e)
+        flow_browser(id, e, browse)
       }
 
       ###################
@@ -155,11 +154,10 @@ flow_run <-
       #################
       # return blocks #
       #################
-      if (node$block_type == "return") {
+      if (data$nodes[data$nodes$id == id, "block_type"] == "return") {
         return(res)
       }
 
       data$edges$arrow[ind] <- "->"
     }
-    data
   }
