de34da6872492daca7946fb5663352cc946dceca
Antoine Fabri <antoine.fabri@gmail.com>
2020-08-06 16:15:32 +0200

    refactor flow_run without subfunctions and without pryr

--- R/06_flow_run.R
+++ R/06_flow_run.R
@@ -17,12 +17,12 @@ flow_run <-
     # e is the env where we evaluate our block and browsing functions
     # it's right under the calling environment and has copies of necessary
     # variables so we don't clutter the fun's environments
-    e <- new.env(parent = parent.frame())
-    e$.flow_blocks_temp_env <- new.env()
-    e$.flow_blocks_temp_env$vars <- list()
-    e$.flow_blocks_temp_env$env <- e$.flow_blocks_temp_env$res <- NULL
-    # to appease cmd check :
-    .flow_blocks_temp_env <- .flow_browser_temp_env <- NULL
+    # we initiate it with the promises of the call
+    e_fun <- fun
+    e_call <- call
+    body(e_fun) <- quote(environment())
+    e_call[[1]] <- e_fun
+    e <- eval.parent(e_call)
 
     # build the diagram data from the function
     flow_data_call <- as.call(list(
@@ -56,59 +56,28 @@ flow_run <-
     })
 
     if (browse) {
-      # .flow_browser_temp_env is used for temp variables in flow_browser
-      e$.flow_browser_temp_env <- new.env()
-      e$.flow_browser_temp_env$code <- code
 
-      flow_browser <- function() {
+      update_diagram <- function() {
         # display updated diagram
-        .flow_browser_temp_env$nomnoml_code <<-
-          build_nomnoml_code(.flow_browser_temp_env$data, code = .flow_browser_temp_env$code, ...)
-        .flow_browser_temp_env$widget_params <<-
-          list(code = .flow_browser_temp_env$nomnoml_code, svg = FALSE)
-        .flow_browser_temp_env$widg <<-
-          htmlwidgets::createWidget(
-          name = "nomnoml", .flow_browser_temp_env$widget_params,
-          package = "nomnoml")
-        print(.flow_browser_temp_env$widg)
-
-        # retrieve all vars so they can override promises when relevant
-        list2env(.flow_blocks_temp_env$vars, environment())
-
-        # browse by reading expressions and evaluating locally, print if relevant
-        repeat {
-          .flow_browser_temp_env$line <<- readline(
-            paste0("flow_browser[", .flow_browser_temp_env$id,"]> "))
-          if (.flow_browser_temp_env$line == "") break
-          .flow_browser_temp_env$res <<-
-            try(withVisible(eval(parse(text = .flow_browser_temp_env$line))))
-          if (!inherits(.flow_browser_temp_env$res, "try-error") &&
-              .flow_browser_temp_env$res$visible)
-            print(.flow_browser_temp_env$res$value)
-        }
-
-        # update our temp environment with all, except for promises
-        .flow_browser_temp_env$vars <<- ls(all.names = TRUE)
-        .flow_browser_temp_env$env  <<- environment()
-        .flow_browser_temp_env$vars <<- Filter(
-          function(x) !is_promise2(as.name(x), .flow_browser_temp_env$env),
-          .flow_browser_temp_env$vars)
-        .flow_browser_temp_env$vars <<- setdiff(.flow_browser_temp_env$vars, "...")
-        # list2env(mget(.flow_browser_temp_env$vars, .flow_browser_temp_env$env),
-        #          .flow_blocks_temp_env$vars)
-        invisible()
+        nomnoml_code  <- build_nomnoml_code(data, code = code, ...)
+        widget_params <- list(code = nomnoml_code, svg = FALSE)
+        widg <- htmlwidgets::createWidget(
+            name = "nomnoml", widget_params, package = "nomnoml")
+        print(widg)
       }
 
-      # we copy formals and build a call so that promises are preserved
-      formals(flow_browser) <- formals(fun)
-      environment(flow_browser) <- e
-      browser_call <- call
-      browser_call[[1]] <- quote(flow_browser)
+      flow_browser <- function(id,e) {
+        repeat {
+          line <- readline(paste0("flow_browser[", id,"]> "))
+          if (line == "") break
+          res <- try(withVisible(eval(parse(text = line), e)))
+          if (!inherits(res, "try-error") && res$visible)
+            print(res$value)
+        }
+      }
     }
 
-
     while (!finished) {
-
       # if we fall on an "end" node (they have negative indices), go to the next one
       if (id < 0) {
         ind <- with(data$edges, from == id & edge_label == "")
@@ -116,41 +85,24 @@ flow_run <-
         data$edges$arrow[ind] <- "->"
       }
 
-      if (browse) {
-
-        e$.flow_browser_temp_env$data <- data
-        e$.flow_browser_temp_env$id <- id
-        eval(browser_call)
-      }
-
       # flag node and extract row of node data from data$nodes into `node`
       data$nodes$entered[data$nodes$id == id] <- TRUE
       node <- as.list(data$nodes[data$nodes$id == id,])
+
+      if (browse) {
+        update_diagram()
+        cat("Next block: ",node$block_type, "\n", node$code_str, "\n")
+        flow_browser(id, e)
+      }
 
       ###################
       # Standard blocks #
       ###################
       if (node$block_type == "standard") {
-        block_fun <- fun
-        body(block_fun) <- bquote({
-          # put all vars in environment
-          list2env(.flow_blocks_temp_env$vars, environment())
 
-          # run code
-          .flow_blocks_temp_env$env <<- environment()
-          .flow_blocks_temp_env$res  <<- .(as.call(c(quote(`{`), node$code[[1]])))
-          .flow_blocks_temp_env$vars <<- ls(all.names = TRUE)
-          .flow_blocks_temp_env$vars <<- setdiff(.flow_blocks_temp_env$vars,  "...")
-          .flow_blocks_temp_env$vars <<- Filter(
-            function(x)  !is_promise2(as.name(x), .flow_blocks_temp_env$env),
-            .flow_blocks_temp_env$vars)
-          .flow_blocks_temp_env$vars <<- mget(.flow_blocks_temp_env$vars)
-          .flow_blocks_temp_env$res
-        })
-        environment(block_fun) <- e
-        temp_call <- call
-        temp_call[[1]] <- quote(block_fun)
-        res <- eval(temp_call)
+        res <- eval(expr  = as.call(c(quote(`{`), node$code[[1]])),
+                    envir = e)
+
         data$nodes$completed[data$nodes$id == id] <- TRUE
         ind <- data$edges$from == id
         id <- data$edges$to[ind]
@@ -160,25 +112,10 @@ flow_run <-
       # if blocks #
       #############
       if (node$block_type == "if") {
-        block_fun <- fun
-        body(block_fun) <- bquote({
-          # put all vars in environment
-          list2env(.flow_blocks_temp_env$vars, environment())
-          # run code
-          .flow_blocks_temp_env$res <<- .(node$code[[1]])
-          # put created vars back in environment
-          .flow_blocks_temp_env$vars <<- ls(all.names = TRUE)
-          .flow_blocks_temp_env$vars <<- setdiff(.flow_blocks_temp_env$vars, "...")
-          .flow_blocks_temp_env$env <<- environment()
-          .flow_blocks_temp_env$vars <<- Filter(
-            function(x)  !is_promise2(as.name(x), .flow_blocks_temp_env$env), .flow_blocks_temp_env$vars)
-          .flow_blocks_temp_env$vars <<- mget(.flow_blocks_temp_env$vars)
-          .flow_blocks_temp_env$res
-        })
-        environment(block_fun) <- e
-        temp_call <- call
-        temp_call[[1]] <- quote(block_fun)
-        res <- eval(temp_call)
+
+        # compute code from new block, e will be modified for next step
+        res <- eval(expr  = as.call(c(quote(`{`), node$code[[1]])),
+                    envir = e)
         data$nodes$completed[data$nodes$id == id] <- TRUE
         if (res) {
           ind <- with(data$edges, from == id & edge_label == "y")
@@ -196,26 +133,10 @@ flow_run <-
       # be different depending on the iteration, so we just undash all arrows
       # and run the full loop
       if (node$block_type %in% c("repeat", "for","while")) {
-        block_fun <- fun
-        body(block_fun) <- bquote({
-          # put all vars in environment
-          list2env(.flow_blocks_temp_env$vars, environment())
-          # run code
-          .flow_blocks_temp_env$res <<- .(as.call(c(quote(`{`), node$code[[1]])))
-          .flow_blocks_temp_env$vars <<- ls(all.names = TRUE)
-          .flow_blocks_temp_env$vars <<- setdiff(.flow_blocks_temp_env$vars, "...")
-          .flow_blocks_temp_env$env <<- environment()
-          .flow_blocks_temp_env$vars <<- Filter(
-            function(x)  !is_promise2(as.name(x), .flow_blocks_temp_env$env),
-            .flow_blocks_temp_env$vars)
-          .flow_blocks_temp_env$vars <<- mget(.flow_blocks_temp_env$vars)
-          .flow_blocks_temp_env$res
-        })
-        environment(block_fun) <- e
-        temp_call <- call
-        temp_call[[1]] <- quote(block_fun)
-        res <- eval(temp_call)
 
+        # compute code from new block, e will be modified for next step
+        res <- eval(expr  = as.call(c(quote(`{`), node$code[[1]])),
+                    envir = e)
 
         id_neg <- -id
         while (length(id)) {
