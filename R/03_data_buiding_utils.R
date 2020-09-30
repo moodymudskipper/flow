82bf341cf31df7470d6f20b5416ea1a89934ca3c
Antoine Fabri <antoine.fabri@gmail.com>
2020-09-30 02:27:43 +0200

    reorganize and rename, add special comments to build_blocks

--- R/03_data_buiding_utils.R
+++ R/03_data_buiding_utils.R
@@ -29,29 +29,6 @@ add_edge <- function(data, to, from = to, edge_label = "", arrow = "->"){
   data
 }
 
-add_data_from_expr <-  function(data, expr, narrow = FALSE){
-  blocks <- build_blocks(expr)
-  for (i in seq_along(blocks)) {
-    block <- blocks[[i]]
-    if (missing(block)) # deal with empty expr (`{}`)
-      block_type <- NULL
-    else
-      block_type <- attr(block, "block_type")
-    if (is.null(block_type)) {
-      data <- add_data_from_standard_block(data, block)
-    } else if (block_type == "if") {
-      data <- add_data_from_if_block(data, block, narrow = narrow)
-    } else if (block_type == "for") {
-      data <- add_data_from_for_block(data, block)
-    } else if (block_type == "while") {
-      data <- add_data_from_while_block(data, block)
-    } else if (block_type == "repeat") {
-      data <- add_data_from_repeat_block(data, block)
-    }
-  }
-  data
-}
-
 add_data_from_standard_block <- function(data, block){
   # increment id
   id <- get_last_id(data) + 1
