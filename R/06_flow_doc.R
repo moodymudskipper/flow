7952158035107ca8240d76c525b3d4e63655fd88
Antoine Fabri <antoine.fabri@gmail.com>
2021-04-16 00:50:08 +0200

    modularize flow_doc

--- R/06_flow_doc.R
+++ R/06_flow_doc.R
@@ -30,15 +30,13 @@ flow_doc <- function(pkg = NULL, out = NULL, prefix = NULL, truncate = NULL,
   if(is.null(out)) {
     ## did we specify a package?
     if(pkgdown) {
-      ## output so pkgdown::build_site will add report on new page on website
+      ## set output so pkgdown::build_site adds report to website
       out <- "diagrams.md"
     } else {
       ## default output
       out <- paste0(pkg, ".html")
     }
   }
-
-  ## fetch lists of exported and uneported functions from package
 
   ## did we specify a package?
   if(pkgdown) {
@@ -73,44 +71,84 @@ flow_doc <- function(pkg = NULL, out = NULL, prefix = NULL, truncate = NULL,
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   ## Write Rmd header
 
-    if(pkgdown)  {
-      rmd_header <- '*This page was built using the [{flow}](https://moodymudskipper.github.io/flow/) package*\n\n'
-    } else {
-      rmd_header <- sprintf('---
-title: "%s"
-output:
-  html_document:
-    toc: true
-    toc_float: true
----
-
-', pkg)
-    }
+  if(pkgdown)  {
+    rmd_header <- ''
+  } else {
+    rmd_header <- sprintf(paste0(
+      '---',
+      'title: "%s"',
+      'output:',
+      '  html_document:',
+      '    toc: true',
+      '    toc_float: true',
+      '---\n\n'), pkg)
+  }
 
   cat(rmd_header, file = rmd_output)
 
+
+  append_function_diagrams(
+    title = "# Exported functions\n\n",
+    progress_txt = "Building diagrams of exported functions\n",
+    funs_split = exported_funs_split,
+    out = rmd_output,
+    ns = ns,
+    path = path,
+    exp_unexp = "exp",
+    ...)
+
+  append_function_diagrams(
+    title = "# Unexported functions\n\n",
+    progress_txt = "Building diagrams of unexported functions\n",
+    funs_split = unexported_funs_split,
+    out = rmd_output,
+    ns = ns,
+    path = path,
+    exp_unexp = "unexp",
+    ...)
+
+  cat("knitting")
+  out <- suppressWarnings(normalizePath(out, winslash = "/"))
+  rmarkdown::render(rmd_output, output_file = out)
+  if(pkgdown) {
+    # remove the "<!DOCTYPE html>" line
+    writeLines(readLines(out)[-1], out)
+  }
+  invisible(NULL)
+}
+
+
+
+
+append_function_diagrams <- function(
+  title,
+  progress_txt,
+  funs_split,
+  out,
+  ns,
+  path,
+  exp_unexp,
+  ...) {
+
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  ## Write "Exported functions" title
-  # Title Exported
-  title_exported <- "# Exported functions\n\n"
-  cat(title_exported, file = rmd_output, append = TRUE)
+  ## Write  title
+  cat(title, file = out, append = TRUE)
 
   ## setup progress bar
-  pb = txtProgressBar(min = 0, max = length(exported_funs), initial = 0)
+  pb = txtProgressBar(min = 0, max = length(unlist(funs_split)), initial = 0)
   stepi = 0
-  cat("Building diagrams of exported functions\n")
+  cat(progress_txt)
 
   ## for every letter
-  for(L in names(exported_funs_split)) {
-
+  for(L in names(funs_split)) {
 
     #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     ## Write letter title
     letter_title <- sprintf("## %s\n\n", L)
-    cat(letter_title, file = rmd_output, append = TRUE)
+    cat(letter_title, file = out, append = TRUE)
 
     ## for every function of the letter group
-    for(fun_chr in exported_funs_split[[L]]) {
+    for(fun_chr in funs_split[[L]]) {
 
       ## fetch function value and subfunctions
       stepi <- stepi + 1
@@ -139,25 +177,25 @@ output:
       }
 
       ## write function title
-      cat(fun_title, file = rmd_output, append = TRUE)
+      cat(fun_title, file = out, append = TRUE)
 
       #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
       # function image
 
       ## build path to image
-      out_tmp <- file.path(path, paste0("exp_", stepi, ".png"))
+      out_tmp <- file.path(path, paste0(exp_unexp, "_", stepi, ".png"))
 
       ## does the function have a body ?
       if(!is.null(body(fun_val))) {
         ## draw to the file and write link to image
         capture.output(suppressMessages(
           flow_view(setNames(c(fun_val), fun_chr),  ..., out = out_tmp)))
-        fun_img <- sprintf('![](exp_%s.png)\n\n', stepi)
-        cat(fun_img, file = rmd_output, append = TRUE)
+        fun_img <- sprintf('![](%s_%s.png)\n\n', exp_unexp, stepi)
+        cat(fun_img, file = out, append = TRUE)
       } else {
         ## write that the function doesn't have a body
         cat("`", fun_chr, "` doesn't have a body\n\n", sep="",
-            file = rmd_output, append = TRUE)
+            file = out, append = TRUE)
       }
 
       ## for all nested functions
@@ -170,7 +208,8 @@ output:
         sub_fun_chr <- names(sub_funs)[[i]]
 
         sub_fun_title <- sprintf("#### %s\n\n", sub_fun_chr)
-        cat(sub_fun_title, file = rmd_output, append = TRUE)
+        cat(sub_fun_title, file = out, append = TRUE)
+
 
         #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         # subfunction image
@@ -182,119 +221,25 @@ output:
         }
 
         ## build path to image
-        out_tmp <- file.path(path, paste0("exp_", stepi,"_", i, ".png"))
+        out_tmp <- file.path(path, paste0(exp_unexp, "_", stepi,"_", i, ".png"))
 
         ## does the function have a body ?
         if(!is.null(body(sub_fun_val))) {
           ## draw to the file and write link to image
           capture.output(suppressMessages(
             flow_view(setNames(c(sub_fun_val), sub_fun_chr),  ..., out = out_tmp)))
-          sub_fun_img <- sprintf('![](exp_%s_%s.png)\n\n', stepi, i)
-          cat(sub_fun_img, file = rmd_output, append = TRUE)
+          sub_fun_img <- sprintf('![](%s_%s_%s.png)\n\n', exp_unexp, stepi, i)
+          cat(sub_fun_img, file = out, append = TRUE)
         } else {
           ## write that the function doesn't have a body
           cat("`", sub_fun_chr, "` doesn't have a body\n\n", sep="",
-              file = rmd_output, append = TRUE)
+              file = out, append = TRUE)
         }
 
       }
-      #nc <- nchar(fun_chr)
     }
   }
 
   ## close progress bar
   close(pb)
-
-  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  # Title Unexported
-  title_unexported <- "# Unexported functions\n\n"
-  cat(title_unexported, file = rmd_output, append = TRUE)
-
-  pb = txtProgressBar(min = 0, max = length(exported_funs), initial = 0)
-  stepi = 0
-  cat("Building diagrams of unexported functions\n")
-
-  for(L in names(unexported_funs_split)) {
-
-    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    # Letter title
-    letter_title <- sprintf("## %s\n\n", L)
-    cat(letter_title, file = rmd_output, append = TRUE)
-
-    for(fun_chr in unexported_funs_split[[L]]) {
-      stepi <- stepi + 1
-      setTxtProgressBar(pb,stepi)
-
-      fun_lng <- str2lang(sprintf("%s::`%s`", pkg, fun_chr))
-      fun_val <- get(fun_chr, envir = ns)
-      sub_funs <- find_funs(body(fun_val))
-      names(sub_funs) <- ifelse(
-        names(sub_funs) == "",
-        seq_along(sub_funs),
-        names(sub_funs)
-      )
-      has_subfuns <- length(sub_funs) > 0
-
-      #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-      # function title
-
-      if(has_subfuns)
-        fun_title <- sprintf('### %s {.tabset}\n\n#### %s\n\n', fun_chr, fun_chr)
-      else
-        fun_title <- sprintf("### %s\n\n", fun_chr)
-
-      cat(fun_title, file = rmd_output, append = TRUE)
-
-      #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-      # function image
-
-      out_tmp <- file.path(path, paste0("unexp_", stepi, ".png"))
-      if(!is.null(body(fun_val))) {
-        capture.output(suppressMessages(
-          flow_view(setNames(c(fun_val), fun_chr),  ..., out = out_tmp)))
-        fun_img <- sprintf('![](unexp_%s.png)\n\n', stepi)
-        cat(fun_img, file = rmd_output, append = TRUE)
-      } else {
-        cat("`", fun_chr, "` doesn't have a body\n\n", sep="",
-            file = rmd_output, append = TRUE)
-      }
-
-      for(i in seq_along(sub_funs)) {
-        sub_fun_val <- eval(sub_funs[[i]])
-        sub_fun_chr <- names(sub_funs)[[i]]
-
-        #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-        # subfunction title
-        sub_fun_title <- sprintf("#### %s\n\n", sub_fun_chr)
-        cat(sub_fun_title, file = rmd_output, append = TRUE)
-
-        #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-        # subfunction image
-        if(grepl("^\\d+$", sub_fun_chr)) sub_fun_chr <- "function"
-        out_tmp <- file.path(path, paste0("unexp_", stepi,"_", i, ".png"))
-
-        if(!is.null(body(sub_fun_val))) {
-          capture.output(suppressMessages(
-            flow_view(setNames(c(sub_fun_val), sub_fun_chr),  ..., out = out_tmp)))
-          sub_fun_img <- sprintf('![](unexp_%s_%s.png)\n\n', stepi, i)
-          cat(sub_fun_img, file = rmd_output, append = TRUE)
-        } else {
-          cat("`", sub_fun_chr, "` doesn't have a body\n\n", sep="",
-              file = rmd_output, append = TRUE)
-        }
-
-      }
-    }
-  }
-  close(pb)
-
-  cat("knitting")
-  out <- suppressWarnings(normalizePath(out, winslash = "/"))
-  rmarkdown::render(rmd_output, output_file = out)
-  if(pkgdown) {
-    # remove the "<!DOCTYPE html>" line
-    writeLines(readLines(out)[-1], out)
-  }
-  invisible(NULL)
 }
-
