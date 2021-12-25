a07088cf8dd43aaa85fbe128ec3ba192def0c813
Antoine Fabri <antoine.fabri@gmail.com>
2021-12-25 22:53:36 +0100

    draft flow_view_golem

--- R/flow_view_golem.R
+++ R/flow_view_golem.R
@@ -13,22 +13,23 @@ flow_view_golem <- function(
   pkg <- namespace_name(ns)
   pkg_objs <- ls(ns)
   pkg_funs <- names(Filter(is.function, mget(pkg_objs, ns)))
-  module_funs <- grep("(_ui)|(_server)$", pkg_objs, value = TRUE)
+  pkg_non_funs <- setdiff(pkg_objs, pkg_funs)
+  module_funs <- grep("(_ui)|(_server)$", pkg_funs, value = TRUE)
   # this should be recursive
   calls_ui_or_server <- sapply(
-    pkg_objs,
+    pkg_funs,
     \(x) any(grepl("(_ui)|(_server)$", all.names(body(getFromNamespace(x, pkg))))))
-  new <- pkg_objs[calls_ui_or_server]
+  new <- pkg_funs[calls_ui_or_server]
   new <- setdiff(new, module_funs)
   module_funs <- c(module_funs, new)
   while(length(new)) {
     new <- Filter(
       \(x) any(all.names(body(getFromNamespace(x, pkg))) %in% module_funs),
-      pkg_objs)
+      pkg_funs)
     new <- setdiff(new, module_funs)
     module_funs <- c(module_funs, new)
   }
-  demote <- paste0(pkg, ":::", setdiff(pkg_objs, module_funs))
+  demote <- paste0(pkg, ":::", c(setdiff(pkg_funs, module_funs), pkg_non_funs))
 
   eval.parent(bquote(
     flow_view_deps(
