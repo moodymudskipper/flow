919a573ab0170f0f9d2253e1423e8c8566be0c48
Antoine Fabri <antoine.fabri@gmail.com>
2022-03-04 19:24:16 +0100

    Call nomnoml::nomnoml to satisfy Ubuntu 18 tests

--- R/flow-package.R
+++ R/flow-package.R
@@ -5,3 +5,8 @@
 #' @importFrom lifecycle deprecated
 ## usethis namespace: end
 NULL
+
+# nomnoml is called only through htmlwidgets::createWidget
+# to pass tests on old ubuntu releases we need to call at least a function once,
+# hence the following line
+nomnoml::nomnoml
